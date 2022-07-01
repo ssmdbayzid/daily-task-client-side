9f3c336b5053d80acbd7bca205a4810c981425b2
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-07-01 16:43:24 +0600

    task move to complete section & delete from to do list

--- src/Pages/CompleteTask/CompleteTask.js
+++ src/Pages/CompleteTask/CompleteTask.js
@@ -1,30 +1,31 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 const CompleteTask = () => {
-
+    const { data: allTask, isLoading, refetch } = useQuery('task', () => fetch('http://localhost:5000/taskComplete').then(res => res.json()))
 
     return (
         <div class="card h-screen w-1/2 mx-auto bg-accent shadow-xl">
             <div class="card-body ">
-                <h2 class=" font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-center ">Task Completed </h2>
-                {/* 
+                <h2 class=" font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-center ">Task Completed {allTask && allTask.length}</h2>
+                
                 <div>
                     {
                         allTask && allTask.map(task =>
                             <div className='shadow-2xl bg-orange-100 p-2 align-center mx-12 flex m-3'>
-                                <input type="checkbox" class="checkbox mr-3" />
+                                <input type="checkbox" class="checkbox mr-3" disabled/>
                                 <h1 className='w-[80%]'>Tast Details {task.task} </h1>
-                                <h1>Edit</h1>
+                                <h1>Completed</h1>
                             </div>
                         )
                     }
 
-                </div> */}
-                <div className='shadow-2xl bg-orange-100 p-2 align-center mx-12 flex m-3'>
+                </div>
+                {/* <div className='shadow-2xl bg-orange-100 p-2 align-center mx-12 flex m-3'>
                     <input type="checkbox" class="checkbox mr-3" />
                     <h1>Tast Details </h1>
                     <h1>Edit</h1>
-                </div>
+                </div> */}
             </div>
         </div>
     );
--- src/Pages/TodoList/TodoList.js
+++ src/Pages/TodoList/TodoList.js
@@ -3,14 +3,13 @@ import { useQuery } from 'react-query';
 import Loading from '../Loading';
 
 const TodoList = () => {
-    
+
     const { data: allTask, isLoading, refetch } = useQuery('task', () => fetch('http://localhost:5000/task').then(res => res.json()))
 
 
     // if(isLoading){
     //     return <Loading></Loading>
     // }
-console.log(allTask)
     const addTask = event => {
         event.preventDefault()
         const task = event.target.task.value;
@@ -21,53 +20,66 @@ console.log(allTask)
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify({task: task})
+            body: JSON.stringify({ task: task })
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch()
+                console.log(data)
+                event.target.task.value = ""
+            })
+
+    }
+
+    const deleteTask = (id) => {
+
+        fetch(`http://localhost:5000/task/${id}`, {
+            method: 'DELETE',
         })
             .then(res => res.json())
             .then(data => {
                 refetch()
                 console.log(data)
             })
-
     }
 
-    const taskComplete = (id) =>{
-
+    const taskComplete = (id) => {
         console.log(id)
         fetch(`http://localhost:5000/taskComplete`, {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify({id})
+            body: JSON.stringify({ id })
         })
             .then(res => res.json())
             .then(data => {
-                refetch()
                 console.log(data)
+                deleteTask(id)
             })
     }
 
     return (
-        <div class="card h-screen w-1/2 mx-auto bg-accent shadow-xl">
-            <div class="card-body ">
-                <h2 class=" font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-center ">To Do List </h2>
-                <form onSubmit={addTask} className='flex mx-8 rounded justify-center'>
-                    <input name='task' className='bg-orange-100 w-[70%] p-2' type="text" placeholder="Add Your Task" />
-                    <input className='text-center bg-orange-500 p-2 btn-outline' type="submit" value="Add Task" />
+        <div className="card min-h-[500px] w-[80%] my-5 md:w-[70%] lg:w-1/2 mx-auto bg-accent shadow-xl">
+            <div className="card-body ">
+
+                <h2 className=" font-bold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-center ">To Do List </h2>
+                <form onSubmit={addTask} className='flex mx-12 justify-center'>
+                    <input name='task' className='bg-orange-100 w-[80%] rounded-l-lg sm:w-full p-2' type="text" placeholder="Add Your Task" required />
+                    <input className='text-center bg-orange-500 rounded-r-lg p-2 btn-outline' type="submit" value="Add Task" />
                 </form>
-                <div>
-                    {
-                        allTask && allTask.map(task=>
-                            <div className='shadow-2xl bg-orange-100 p-2 align-center mx-12 flex m-3'>
-                            <input onClick={()=>taskComplete(task._id)} type="checkbox" class="checkbox mr-3" />
-                            <h1 className='w-[80%]'>Tast Details {task.task} </h1>
+                {/* <div> */}
+                {
+                    allTask && allTask.map(task =>
+                        <div className='shadow-2xl w-full lg:w-[80%] bg-orange-100 p-2 align-center mx-auto flex my-1/2 rounded'>
+                            <input onClick={() => taskComplete(task._id)} type="checkbox" className="checkbox mr-3" />
+                            <h1 className='w-[70%]'>Tast Details {task.task} </h1>
                             <h1>Edit</h1>
                         </div>
-                        )
-                    }
-                    
-                </div>
+                    )
+                }
+
+                {/* </div> */}
             </div>
         </div>
     );
